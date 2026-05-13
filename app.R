c5c8336f5653190bb39b8b67423b53b53a20b594
Hjalte Parner <hjalte@ices.dk>
2026-05-13 11:33:48 +0200

    Replace initial shiny app with the COMPEAT shiny app - without assessment data results

--- app.R
+++ app.R
@@ -1,51 +1,94 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+library(bslib)
+library(data.table)
+library(DT)
+library(htmltools)
+library(leaflet)
+library(leaflet.extras)
+library(R.methodsS3)
+library(R.oo)
+library(R.utils)
+library(sf)
 library(shiny)
+library(shinycssloaders)
+library(shinyjs)
+library(stringr)
+library(tidyverse)
+library(yaml)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+source("./app_helpers.R")
+source("./app_stations.R")
+source("./app_annual_indicators.R")
+source("./app_assessment_indicators.R")
+source("./app_assessment.R")
+glossary <- readRDS("./app/glossary.rds")
+station_configuration <- read_yaml("./app/stations_config.yml")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- tagList(
+  tags$script(HTML("
+    $(document).ready(function() {
+      $('[data-toggle=\"tooltip\"]').tooltip();
+    });
+  ")),
+  tags$head(tags$link(rel = "stylesheet", type = "text/css",
+                      href = "./styles.css")),
+  navbarPage(
+    position = "static-top",
+    collapsible = TRUE,
+    windowTitle = "COMPEAT",
+    id = "tabset",
+    fluid = TRUE,
+    theme = bslib::bs_theme(bootswatch = "flatly"),
+    title = span("Commom Procedure Eutrophication Assessment Tool (COMPEAT)"),
+    tabPanel(
+      "Assessment",
+      moduleAssessmentUI("Assessment")
+    ),
+    tabPanel(
+      "Indicators",
+      tabsetPanel(
+        moduleAssessmentIndicatorsUI("AssessInd"),
+        moduleAnnualIndicatorsUI("AnnualInd")
+      )
+    ),
+    tabPanel(
+      "Stations",
+      moduleStationsUI("Stations")
+    ),
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  shared_state <- reactiveValues(assessment = NULL)
+  
+  # Fetch available assessments
+  available_assessments <- list.dirs("./Data",
+                                     recursive = FALSE,
+                                     full.names = FALSE) %>% sort(decreasing = TRUE)
+  
+  # Initialize shared_state$assessment with the first available assessment
+  observe({
+    if (is.null(shared_state$assessment) && length(available_assessments) > 0) {
+      shared_state$assessment <- available_assessments[1]
+    }
+  })
+  
+  # Optional: If no assessments are available, handle accordingly
+  observe({
+    if (length(available_assessments) == 0) {
+      showModal(modalDialog(
+        title = "No Assessments Found",
+        "Please add assessments to the ./Data directory.",
+        easyClose = TRUE,
+        footer = NULL
+      ))
+    }
+  })
+  
+  # Initialize Modules without their own assessment selectors
+  moduleAssessmentServer("Assessment", shared_state = shared_state, glossary = glossary)
+  moduleAssessmentIndicatorsServer("AssessInd", shared_state = shared_state, glossary = glossary)
+  moduleAnnualIndicatorsServer("AnnualInd", shared_state = shared_state, glossary = glossary)
+  moduleStationsServer("Stations", shared_state = shared_state, station_configuration = station_configuration)
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
